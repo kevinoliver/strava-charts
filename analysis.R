977807e68ffd4799b623df96c5e9cfce1fd4ac77
Kevin Oliver <kevinoliver@gmail.com>
2023-11-03 11:12:06 -0700

    heart rate to pace

--- analysis.R
+++ analysis.R
@@ -33,6 +33,8 @@ activities$DistanceMiles <- activities$Distance / 1.609
 activities$SecondsPerMile <- activities$`Moving.Time` / activities$DistanceMiles
 # Elevation in Feet
 activities$ElevationGainFeet <- activities$`Elevation.Gain` * 3.281
+# Heart rate to pace
+activities$HeartRateToPace <- activities$`Average.Heart.Rate` / activities$SecondsPerMile
 
 # Filter down to just runs (and after i "started" running in August)
 runs <- activities[activities$ActivityType == 'Run', ]
@@ -208,3 +210,26 @@ ggplot(data = runs, aes(x = as.Date(ActDate), y = cumsum(Distance))) +
   scale_y_continuous(expand = expansion(mult = c(0, 0.1))) # removes the padding under the x-axis
 dev.off()
 
+# Plot heart rate to pace.
+png("~/downloads/heartratetopace.png", units="in", width=6, height=4, res=300)
+ggplot(data = runs, mapping = aes(x = as.Date(ActDate), y = HeartRateToPace)) +
+  geom_point(stat = "identity", show.legend = FALSE, color = "darkgoldenrod1") +
+  geom_smooth(method = "gam", show.legend = FALSE, se = FALSE, color = "darkgoldenrod3") +
+  labs(
+    title = "Heart rate to pace",
+    subtitle = "Beats per minute / seconds per mile",
+    x = element_blank(),
+    y = element_blank()) +
+  theme_light() +
+  theme(
+    plot.title = element_text(colour = "goldenrod4", face = "bold"),
+    plot.subtitle = element_text(colour = "goldenrod4", face = "bold"),
+    axis.text.x = element_text(colour = "goldenrod4", size = 10, angle = 90, hjust = 1, vjust = 0.5),
+    axis.text.y = element_text(colour = "goldenrod4", size = 10),
+    axis.ticks = element_blank(), # remove tick marks
+    panel.grid.major.x = element_blank(), # remove vertical grid lines
+    panel.border = element_blank() # remove border around graph
+  ) +
+  scale_x_date(date_breaks = "3 months", date_labels = "%b %Y", minor_breaks = NULL) +
+  scale_color_identity(breaks = c("darkgoldenrod1", "darkgoldenrod3"))
+dev.off()
